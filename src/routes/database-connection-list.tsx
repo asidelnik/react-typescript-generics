46b6bccd18a79ca8f528f1256ff48ab743b582d4
Amos <asidelnik@gmail.com>
2024-05-21 13:51:27 +0300

    DatabaseConnectionList - added databases table

--- src/routes/database-connection-list.tsx
+++ src/routes/database-connection-list.tsx
@@ -1,10 +1,37 @@
+import { baseUrl, serverRoutes } from "../constants/server";
 import useServerData from "../custom-hooks/useServerData";
+import { IDatabase } from "../types/IDatabase";
+import { ServerStatus } from "../enums/ServerStatus";
 
 export default function DatabaseConnectionList() {
-  const { serverStatus, data } = useServerData();
+  const { serverStatus, data } = useServerData(baseUrl + serverRoutes.databases);
   return (
     <>
-      <div>Database connection list {serverStatus}</div>
+      <h3>Database connection list</h3>
+      {serverStatus === ServerStatus.Success ? (
+        <table>
+          <thead>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Type</th>
+          </thead>
+          <tbody>
+            {data.map((item: IDatabase) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.username}</td>
+                  <td>{item.type}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      ) : (
+          <>
+          {serverStatus === ServerStatus.Error && <h4>Server error</h4>}
+        </>
+      )}
     </>
   )
 }

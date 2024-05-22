fee690091e4b4b2b540ffce23c41f5e953ba8563
Amos <asidelnik@gmail.com>
2024-05-22 19:22:53 +0300

    React router - error page - added

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import DatabaseDetails from "./routes/database-details";
 import DatabaseConnectionList from "./routes/database-connection-list";
+import ErrorPage from "./routes/error-page";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <DatabaseConnectionList />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "database-details/:databaseId",
     element: <DatabaseDetails />,
+    errorElement: <ErrorPage />,
   },
 ]);
---
+++ src/routes/error-page.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export default function ErrorPage() {
+  return (
+    <>
+      <h3>Error page</h3>
+      <Link to="/">Go to home page</Link>
+    </>
+  )
+}

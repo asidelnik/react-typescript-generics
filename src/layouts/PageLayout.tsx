1fd6b929b20ccc18f9958a20ce1a32da3d0b61f9
Amos <asidelnik@gmail.com>
2024-05-23 18:38:52 +0300

    Page title calculated from pathname.

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,14 +1,19 @@
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import PageNavigation from "../navigations/PageNavigation";
+import { pageLinks } from "../constants/pageLinks";
 
 export default function PageLayout() {
   const { page } = useParams<{ page: string }>();
+  const { pathname } = useLocation();
+  const isItemsSection = !pathname.includes('item-details');
+  const selectedPage = pageLinks.find(p => p.name === page);
+  const pageTitle = selectedPage ? isItemsSection ? selectedPage.itemsLabel : selectedPage.detailsLabel : 'Page not found';
   return (
     <>
       <PageNavigation />
       <main className="page-layout">
         <header className="page-title">
-          <h3>{page}</h3>
+          <h3>{pageTitle}</h3>
         </header>
         <section className="content-container">
           <Outlet />

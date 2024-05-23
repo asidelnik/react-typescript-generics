41d98aa23b8df281c322ebd718408715294c28fa
Amos <asidelnik@gmail.com>
2024-05-23 21:44:44 +0300

    Row key update

--- src/shared/components/RowColumns.tsx
+++ src/shared/components/RowColumns.tsx
@@ -6,10 +6,10 @@ export default function RowColumns({ rowData, columns }: IRowColumns) {
   const navigate = useNavigate();
   const dataRows: Array<string | number> = columns.map(c => rowData[c.name])
   const { pathname } = useLocation();
-  const firstPath = pathname.split('/')[1];
+  const pageName = pathname.split('/')[1];
 
   function handleRowClick() {
-    const path = `/${firstPath}/${clientRoutes.details}/${rowData.id}`;
+    const path = `/${pageName}/${clientRoutes.details}/${rowData.id}`;
     navigate(path);
   }
 
@@ -18,7 +18,7 @@ export default function RowColumns({ rowData, columns }: IRowColumns) {
       <tr onClick={handleRowClick}>
         {dataRows.map((column: number | string, index: number) => {
           return (
-            <td key={rowData.id + '-' + column} className={index === 0 ? 'table-link' : ''}>
+            <td key={pageName + '-' + rowData.id + '-' + index} className={index === 0 ? 'table-link' : ''}>
               {column}
             </td>)
         })}

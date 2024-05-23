5f1345623e0b2b2d770cb959f9b116293c041930
Amos <asidelnik@gmail.com>
2024-05-24 00:27:02 +0300

    package.json - deploy script. Deploy baseUrl added.

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,4 +1,7 @@
+//// Local
 export const baseUrl = 'http://localhost:4000/';
+//// Deploy - GitHub Pages
+//export const baseUrl = 'https://my-json-server.typicode.com/asidelnik/database-connection-manager-server/';
 
 export enum clientRoutes {
   details = 'item-details',
